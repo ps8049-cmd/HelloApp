a3fe6983aad4266202c1b41e6d9d74db0385ee02
ps8049 <ps8049@srmist.edu.in>
2026-03-18 13:38:19 +0530

    UC3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,6 +1,17 @@
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        String name;
+
+        // Check if user passed argument
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        // Print greeting
+        System.out.println("Hello " + name);
     }
 }
