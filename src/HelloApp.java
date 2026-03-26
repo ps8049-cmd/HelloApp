81d97e88c2575fbfa6b914267e988723df35d805
ps8049 <ps8049@srmist.edu.in>
2026-03-26 13:20:37 +0530

    UC7

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,19 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+       
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
+        } else {
+           
+            String names = String.join(", ", args);
+
+            
+            System.out.println("Hello " + names);
         }
-        
-        String result = "Hello ";
-
-        for (String name : args) {
-            result += name + ", ";
-        }
-
-        result = result.substring(0, result.length() - 2);
-
-        System.out.println(result);
     }
 }
