a83c0008d206f368af2e70fa5cdbe3b8017a154d
ps8049 <ps8049@srmist.edu.in>
2026-03-26 13:00:50 +0530

    UC5

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,15 +1,12 @@
 public class HelloApp {
-
     public static void main(String[] args) {
 
-        // If no arguments, print default
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } 
         else {
-            // Loop through all names
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
             }
         }
     }
